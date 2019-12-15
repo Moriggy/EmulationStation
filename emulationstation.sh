b698dbd98c720d20a945fbea2ec05fed05e9441f
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-12-15 18:37:01 +0100

    update
    cambio de iconos y añadidos videos reinicio y apagado

--- emulationstation.sh
+++ emulationstation.sh
@@ -8,11 +8,13 @@ while true; do
     [ -f /tmp/es-restart ] && continue
     if [ -f /tmp/es-sysrestart ]; then
         rm -f /tmp/es-sysrestart
+		sudo omxplayer -b EmulOS/shutdown/Reiniciando.mp4
         sudo reboot
         break
     fi
     if [ -f /tmp/es-shutdown ]; then
         rm -f /tmp/es-shutdown
+		sudo omxplayer -b EmulOS/shutdown/Apagando.mp4
         sudo poweroff
         break
     fi
